6749e3992c684b7681381576fee6b3b5b1bec6a5
Evandro Florscuk <evandro_ff@me.com>
2024-04-13 12:28:32 -0300

    :recycle::chore: Update index.js

--- verificador-nota/index.js
+++ verificador-nota/index.js
@@ -6,31 +6,6 @@ const app = express();
 app.use(express.json());
 app.use('/valida/:cnpj', routes);
 
-// const CNPJ_MADEIRA = '10490181000569';
-
-// app.get('/valida/:cnpj', (req, res) => {
-//     try {
-//         const { cnpj } = req.params;
-    
-//         if (!cnpj) {
-//             throw new Error('CNPJ não fornecido na URL');
-//         }
-
-//         const isValid = cnpj === CNPJ_MADEIRA;
-
-//         if (!isValid) {
-//             const horaAtual = new Date().toLocaleString();
-//             console.log(`CNPJ inválido ${cnpj} inserido em ${horaAtual}`);
-//         }
-
-//         res.json({ isValid });
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// });
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
