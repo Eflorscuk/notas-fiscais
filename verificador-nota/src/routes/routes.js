7f4d4366b0efc6e6d499c72ad1add31931f0e2fd
Evandro Florscuk <evandro_ff@me.com>
2024-04-14 22:43:35 -0300

    :construction: work in verificador-notas

--- verificador-nota/src/routes/routes.js
+++ verificador-nota/src/routes/routes.js
@@ -3,6 +3,10 @@ const { validaCnpj } = require('../controller/valida.controller')
 
 const router = express.Router();
 
-router.get('/:cnpj', validaCnpj);
+// router.get('/:cnpj', validaCnpj);
+router.get('/:cnpj', ()=>{
+    console.log('routes')
+});
+
 
 module.exports = router;
